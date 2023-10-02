7c6469bdb642ce912003ba3870990cc28c6f9efe
Erica Mae De Vera <deveraericamae@gmail.com>
2023-10-02 10:43:56 +0800

    [FEAT] DOWNLOAD BUTTON

--- src/components/DashboardComponents/FileComponent/FileComponent.jsx
+++ src/components/DashboardComponents/FileComponent/FileComponent.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, shallowEqual } from "react-redux";
-import { Button } from 'antd';
+
 import Header from "./Header";
 import CodeEditor from "./CodeEditor";
-import DocumentViewer from "../FileComponent/DocumentViewer";
 
 const FileComponent = () => {
   const { fileId } = useParams();
@@ -35,46 +34,18 @@ const FileComponent = () => {
     }
   }, [currentFile, currentFile?.data?.data]);
 
-  const renderFilePreview = () => {
-    const fileExtension = currentFile?.data?.extension.toLowerCase();
+  const downloadFile = () => {
+    const element = document.createElement("a");
+    element.setAttribute("href", currentFile?.data?.url);
+    element.setAttribute("download", currentFile?.data?.name);
+    element.setAttribute("target", "_blank");
+    element.style.display = "none";
+    document.body.appendChild(element);
 
-    if (fileExtension.includes("png") || fileExtension.includes("jpg") || fileExtension.includes("jpeg") || fileExtension.includes("gif")) {
-      // Display images as you were doing
-      return (
-        <img
-          src={currentFile?.data?.url}
-          alt={currentFile?.data?.name}
-          className="w-100 h-100 img-fluid"
-        />
-      );
-    } else if (fileExtension.includes("pdf")) {
-      // Use a PDF viewer component for PDF files
-      return <div>PDF Viewer Goes Here</div>;
-    } else if (fileExtension.includes("doc") || fileExtension.includes("docx")) {
-      // Use the DocumentViewer component for document files
-      return <DocumentViewer url={currentFile?.data?.url} />;
-    } else if (fileExtension.includes("txt")) {
-      // Display the content of a TXT file as text
-      return (
-        <div className="w-100 h-100 p-3">
-          <pre>{currentFile?.data?.data}</pre>
-        </div>
-      );
-    } else if (fileExtension.includes("xls") || fileExtension.includes("xlsx")) {
-      // Use an Excel viewer component for Excel files
-      return <ExcelViewer url={currentFile?.data?.url} />;
-    } else {
-      return (
-        <div className="w-100 h-100 d-flex justify-content-center align-items-center">
-          <p className="text-center">
-            File type not supported. Please download the file to view it.
-          </p>
-        </div>
-      );
-    }
+    element.click();
+
+    document.body.removeChild(element);
   };
-
-
 
   if (isAuthenticated)
     return (
@@ -94,34 +65,49 @@ const FileComponent = () => {
             />
           </>
         ) : (
-          <div className="position-fixed left-0 top-0 w-100 h-50 bg-black text-white">
-            <div className="d-flex py-1 mt-4 px-5 justify-content-between align-items-center">
+          <div className="position-fixed left-0 top-0 w-100 h-100 bg-black text-white">
+            {/* sub menu bar   */}
+            <div className="d-flex py-4 mt-4 px-5 justify-content-between align-items-center">
               <p title={currentFile?.data?.name} className="my-0">
                 {currentFile?.data?.name.length > 40
                   ? currentFile?.data?.name.slice(0, 40) +
-                  "... ." +
-                  currentFile?.data?.extension
+                    "... ." +
+                    currentFile?.data?.extension
                   : currentFile?.data?.name}
               </p>
               <div className="d-flex align-items-center me-5">
-                <Button
-                  type="default"
+                <button
+                  className="btn btn-sm btn-outline-light me-2"
                   onClick={() => navigate(-1)}
-                  style={{ marginRight: '10px' }} // Add margin to the right
                 >
                   Go Back
-                </Button>
-                <Button
-                  type="primary"
+                </button>
+                <button
+                  className="btn btn-sm btn-primary"
                   onClick={() => downloadFile()}
                 >
                   Download
-                </Button>
+                </button>
               </div>
-
             </div>
             <div className="w-100 mt-4" style={{ height: "650px" }}>
-              {renderFilePreview()}
+              {currentFile?.data?.extension.includes("png") ||
+              currentFile?.data?.extension.includes("jpg") ||
+              currentFile?.data?.extension.includes("jpeg") ||
+              currentFile?.data?.extension.includes("gif") ? (
+                <img
+                  src={currentFile?.data?.url}
+                  alt={currentFile?.data?.name}
+                  className="w-100 h-100 img-fluid"
+                />
+              ) : (
+                <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+                  <p className="text-center">
+                    File type not supported. Please download the file to view
+                    it.
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         )}
